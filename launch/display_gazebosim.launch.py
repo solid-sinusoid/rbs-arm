97b7093d38d8de257d9297876c22c67493def4fb
Bill Finger <ur.narmak@gmail.com>
2025-02-06 15:30:54 +0300

    Migrate to new robot structure, refactor URDF, and update GazeboSim launch
    - Transitioned to a new robot structure with updated URDF and Xacro definitions.
    - Updated `rbs_ee_link.xacro`, `rbs_fork_link.xacro`, and `rbs_main_link.xacro`:
      - Adjusted joint limits, positions, and axes.
      - Standardized joint parameters and transformations.
      - Updated inertia, mass, and mesh references.
      - Added `initial_joint_position` for better initialization.
    - Refactored `rbs_arm_modular_macro.xacro`:
      - Simplified link definitions and removed legacy gripper integration.
      - Improved Gazebo compatibility.
    - Updated `sensors.xacro` to use `gz_ros2_control/GzFts` instead of `IgnitionFts`.
    - Removed outdated URDF files (`rbs_arm.urdf`, `rbs_arm.xacro`, etc.).
    - Refactored `display_gazebosim.launch.py`:
      - Introduced `OpaqueFunction` and `load_xacro_args` for improved launch setup.
      - Enhanced robot description processing using `xacro.process_file`.
      - Added a clock bridge node for time synchronization.
    - Updated URDF/Xacro files:
      - Introduced `initial_joint_position` in `joint_hardware.xacro` and `joint_interface.xacro`.
      - Refactored `rbs_base_link.xacro` for improved formatting and efficiency.
    - Modified collision and visual mesh files:
      - Added new STL files for individual axes and base link.
      - Removed outdated STL files (`ee_link.stl`, `fork_link.stl`, etc.).

--- launch/display_gazebosim.launch.py
+++ launch/display_gazebosim.launch.py
@@ -1,13 +1,140 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, OpaqueFunction
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
+import xacro
 
-from rbs_arm import RbsBuilder
+from robot_builder.external.ros2_control import ControllerManager
+from robot_builder.parser.urdf import URDF_parser
+from rbs_utils.launch import load_xacro_args
+
+
+def launch_setup(context, *args, **kwargs):
+    tf_prefix = LaunchConfiguration("tf_prefix").perform(context)
+    description_package = LaunchConfiguration("description_package").perform(context)
+    description_file = LaunchConfiguration("description_file").perform(context)
+    hardware = LaunchConfiguration("hardware").perform(context)
+
+
+    description_package_abs_path = get_package_share_directory(
+        description_package
+    )
+
+    controllers_file = os.path.join(
+        description_package_abs_path, "config", "controllers.yaml"
+    )
+
+    xacro_file = os.path.join(
+        description_package_abs_path,
+        "urdf",
+        description_file
+    )
+
+    xacro_config_file = os.path.join(
+        description_package_abs_path, "urdf", "xacro_args.yaml"
+    )
+
+
+    mappings_data = load_xacro_args(xacro_config_file, locals())
+
+    robot_description_doc = xacro.process_file(xacro_file, mappings=mappings_data)
+
+    
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", "view_robot.rviz"]
+    )
+    robot_description_content = robot_description_doc.toprettyxml(indent="  ")
+    robot_description = {"robot_description": robot_description_content}
+
+    # with open(f"rbs_g_arm0_6.urdf", 'w') as xfile:
+    #     xfile.write(robot_description_content)
+    #     xfile.close()
+
+
+    robot = URDF_parser.load_string(
+        robot_description_content, ee_link_name="ee_link"
+    )
+    ControllerManager.save_to_yaml(
+        robot, description_package_abs_path, "controllers.yaml"
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "-c", "/controller_manager",
+        ],
+    )
+
+    joint_trajectory_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_trajectory_controller",
+            "-c", "/controller_manager",
+        ],
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[{"use_sim_time": True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_gz_sim'),
+                            'launch', 'gz_sim.launch.py')]),
+            launch_arguments=[('gz_args', [' -r ', "empty.sdf"])],
+    )
+    # Spawn robot
+    gazebo_spawn_robot = Node(
+        package='ros_gz_sim', 
+        executable='create',
+        arguments=[
+            '-name', "arm0",
+            '-x', '0.0',
+            '-z', '0.0',
+            '-y', '0.0',
+            '-topic', "/robot_description"
+        ],
+        output='screen',
+        parameters=[{"use_sim_time": True}]
+    )
+
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
+    )
+
+    nodes_to_start = [
+        joint_state_broadcaster_spawner,
+        joint_trajectory_controller,
+        robot_state_publisher_node,
+        rviz_node,
+        gazebo,
+        gazebo_spawn_robot,
+        clock_bridge
+    ]
+
+    return nodes_to_start
 
 def generate_launch_description():
     declared_arguments = []
@@ -44,97 +171,7 @@ def generate_launch_description():
             description="The name of hardware", 
         )
     )
-    tf_prefix = LaunchConfiguration("tf_prefix")
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
-    hardware = LaunchConfiguration("hardware")
 
-    
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", "view_robot.rviz"]
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
     )
-    # initial_joint_controllers = PathJoinSubstitution(
-    #     [FindPackageShare("rbs_arm"), "config", "rbs_arm_controllers_gazebosim.yaml"]
-    # )
-    namespace = "/arm0"
-    robot = RbsBuilder(ndof=6, robot_name="arm0", parent="world", gripper_package="rbs_gripper")
-    robot.base()
-    robot.gripper()
-    robot.ros2_control("gazebo")
-    robot.simulation()
-    robot.moveit()
-    robot_description_content = robot.robot.urdf().urdf()
-    # urdf = open("/home/bill-finger/rbs_ws/src/rbs_arm/rbs_arm_6.urdf").read()
-    # print(urdf)
-    robot_description = {"robot_description": robot_description_content}
-
-    # with open(f"rbs_g_arm0_6.urdf", 'w') as xfile:
-    #     xfile.write(robot_description_content)
-    #     xfile.close()
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        namespace=namespace,
-        arguments=[
-            "joint_state_broadcaster",
-            "-c", namespace + "/controller_manager",
-        ],
-    )
-
-    joint_trajectory_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        namespace=namespace,
-        arguments=[
-            "joint_trajectory_controller",
-            "-c", namespace + "/controller_manager",
-        ],
-    )
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        namespace="/arm0",
-        output="both",
-        parameters=[robot_description],
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-    )
-
-    gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('ros_gz_sim'),
-                            'launch', 'gz_sim.launch.py')]),
-            launch_arguments=[('gz_args', [' -r ', "empty.sdf"])],
-    )
-    # Spawn robot
-    gazebo_spawn_robot = Node(
-        package='ros_gz_sim', 
-        executable='create',
-        arguments=[
-            '-name', "arm0",
-            '-x', '0.0',
-            '-z', '0.0',
-            '-y', '0.0',
-            '-topic', namespace + "/robot_description"
-        ],
-        output='screen'
-    )
-
-    nodes_to_start = [
-        joint_state_broadcaster_spawner,
-        joint_trajectory_controller,
-        robot_state_publisher_node,
-        rviz_node,
-        gazebo,
-        gazebo_spawn_robot
-    ]
-
-    return LaunchDescription(declared_arguments + nodes_to_start)
